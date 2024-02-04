e4ad7f2fff0ff6354f074f882cc07ed8f9840a15
Tuan Ha <hatuan@email.cz>
2024-02-04 13:07:47 +0100

    add turn and game over, game has players and gameboard

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -1,8 +1,15 @@
 class GameBoard
-  attr_accessor :gameboard
+  attr_accessor :gameboard, :isGameOver
+
+  @@WIN_CONDITIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
+    [0, 4, 8], [2, 4, 6]             # diagonal
+  ]
 
   def initialize()
     @gameboard = Array.new(9, " ")
+    @isGameOver = false
   end
 
   def print_gameboard()
@@ -17,13 +24,42 @@ class GameBoard
 end
 
 class Player
-  attr_accessor :mark, :name
+  attr_accessor :mark, :name, :turn
 
   def initialize(name, mark)
     @name = name
     @mark = mark
+    @turn = false
   end
 end
 
+class Game
+  attr_accessor :board, :player1, :player2
+
+  def initialize(board, player1, player2)
+    @board = board
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def run()
+    @board.print_gameboard
+    select_place(2)
+    @board.print_gameboard
+    select_place(6)
+    @board.print_gameboard
+  end
+
+  def select_place(index) 
+    @board.gameboard[index] = "X"
+  end
+end
+
+
+
+playerX = Player.new("Black", "X")
+player0 = Player.new("Niger", "O")
 gameboard = GameBoard.new()
-gameboard.print_gameboard()
+
+game = Game.new(gameboard, playerX, player0)
+game.run()
