baac576c602cb7aff6119bd7a9df8a396e98d786
Tuan Ha <hatuan@email.cz>
2024-02-08 17:43:25 +0100

    make it case-insensitive

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -56,10 +56,12 @@ class HumanPlayer
   def get_answer()
     answer = gets
     answer_array = answer.split(" ")
+    answer_array = answer_array.map { |color| color.upcase}
       while answer_array.length != 4
         puts "You have to write 4 letter representing color in format : R G B Y"
         answer = gets
         answer_array = answer.split(" ")
+        answer_array = answer_array.map { |color| color.upcase}
       end
     answer_array
   end
