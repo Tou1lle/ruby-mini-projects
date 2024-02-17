9818bf6ae82c139d84bc6944961f0e3d10c4cb75
Tuan Ha <hatuan@email.cz>
2024-02-17 15:52:01 +0100

    use only 5-12 length words

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,2 +1,5 @@
-google_words = File.read("google-10000-english-no-swears.txt")
-hangman_dictionary = google_words.split
+google_words = File.readlines("google-10000-english-no-swears.txt")
+hangman_words = google_words.select do | word |
+  word.strip!
+  (word.length >= 5 && word.length <= 12)
+end
