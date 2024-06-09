d8052b1fb8ddcc76bda35402e1c9330197d93d8a
Tuan Ha <hatuan@email.cz>
2024-06-09 14:08:04 +0200

    include rubocop, make changes

--- ceasar-cipher/script.rb
+++ ceasar-cipher/script.rb
@@ -3,49 +3,45 @@
 # a = 97, A = 65
 # z = 122, Z = 90
 
-=begin
- this methods is responsible for ciphering the string with the key
- key is int
-=end
+#  this methods is responsible for ciphering the string with the key
+#  key is int
 def caesar_cipher(string, key)
-    string_codepoints = string.codepoints
+  string_codepoints = string.codepoints
 
-    codepoints_with_key = increment_codepoint(string_codepoints, key)
+  codepoints_with_key = increment_codepoint(string_codepoints, key)
 
-    ciphred_key = codepoints_with_key.map { |n| n.chr}
-    ciphred_key.join
+  ciphred_key = codepoints_with_key.map(&:chr)
+  ciphred_key.join
 end
-
 
 # increment the codepoint value by the key
-def increment_codepoint(array, key) 
-    incremented_array = []
+def increment_codepoint(array, key)
+  incremented_array = []
 
-    array.each do | codepoint |
-        if !(codepoint.between?(65, 90) || codepoint.between?(97, 122))
-            incremented_array.push(codepoint)
-        else
-            for i in 1..key
-                codepoint += 1
-                codepoint = check_overlap(codepoint)
-                incremented_array.push(codepoint) if i == key
-            end
-        end
+  array.each do |codepoint|
+    if codepoint.between?(65, 90) || codepoint.between?(97, 122)
+      (1..key).each do |n|
+        codepoint += 1
+        codepoint = check_overlap(codepoint)
+        incremented_array.push(codepoint) if n == key
+      end
+    else
+      incremented_array.push(codepoint)
     end
+  end
 
-    incremented_array
+  incremented_array
 end
-
 
 # Check if the value is is in the alphabet
 def check_overlap(codepoint)
-    if codepoint == 123
-        return 97
-    elsif codepoint == 91
-        return 65
-    else
-        return codepoint
-    end
+  if codepoint == 123
+    97
+  elsif codepoint == 91
+    65
+  else
+    codepoint
+  end
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 1)
